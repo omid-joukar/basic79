5174faae6c82dd86b1b53811f9d4e966c7cf481c
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-13 10:11:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/**
+ * Created by KPS on 7/13/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        int[] array_nums = {20, 30, 40};
+        System.out.println("Original Array: "+ Arrays.toString(array_nums));
+        int[] new_array_nums = {array_nums[1], array_nums[2], array_nums[0]};
+        System.out.println("Rotated Array: "+Arrays.toString(new_array_nums));
+    }
+}
+
